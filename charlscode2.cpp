7790e040bea1fed18fcf3a3b795eded58eee6d2e
Charlesbieg <chitimbwacharles100@gmail.com>
2024-12-22 18:47:11 +0300

    Add files via upload

---
+++ charlscode2.cpp
@@ -0,0 +1,10 @@
+class Hotel {
+public:
+    void showMenu() {
+        cout << "***** Hotel Management System *****\n";
+        cout << "1. Show Available Rooms\n";
+        cout << "2. Book a Room\n";
+        cout << "3. Exit\n";
+        cout << "Enter your choice: ";
+    }
+};
