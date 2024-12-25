541e3166428b8580408ee21e250ac8a4a6ba05cf
Charlesbieg <chitimbwacharles100@gmail.com>
2024-12-25 15:46:43 +0300

    Add files via upload

---
+++ charlscode9.cpp
@@ -0,0 +1,9 @@
+void showMenu() {
+    cout << "***** Hotel Management System *****\n";
+    cout << "1. Show Available Rooms\n";
+    cout << "2. Book a Room\n";
+    cout << "3. Checkout from Room\n";
+    cout << "4. Show Room Details\n";
+    cout << "5. Exit\n";
+    cout << "Enter your choice: ";
+}
