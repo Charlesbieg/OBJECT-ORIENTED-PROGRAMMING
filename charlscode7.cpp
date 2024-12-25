776539441f1da7a2b3a242d1da67a463e60c717f
Charlesbieg <chitimbwacharles100@gmail.com>
2024-12-25 15:45:29 +0300

    Add files via upload
    Seven commit for system of hotel management

---
+++ charlscode7.cpp
@@ -0,0 +1,8 @@
+void showMenu() {
+    cout << "***** Hotel Management System *****\n";
+    cout << "1. Show Available Rooms\n";
+    cout << "2. Book a Room\n";
+    cout << "3. Checkout from Room\n";
+    cout << "4. Exit\n";
+    cout << "Enter your choice: ";
+}
