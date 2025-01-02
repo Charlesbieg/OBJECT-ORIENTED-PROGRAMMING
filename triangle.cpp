124319d72a629679fd8d3a1df951ba400248b325
Charlesbieg <chitimbwacharles100@gmail.com>
2025-01-02 22:26:08 +0300

    Add files via upload

---
+++ triangle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float num1,  num2;
+    cout << "----Calculator to findbarea of triangle----" << endl;
+    cout << "Enter Height of Areaa: ";
+    cin >> num1;
+    cout << "Enter Width of Area:  " ;
+    cin >> num2;
+    cout << "Area is: "<<0.5 * (num1/ num2);
+}
